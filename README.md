e63303e25c989e71dd4921d66556cc1a1e137546
Mehmet Sezer <m.szr98@gmail.com>
2020-12-21 19:32:19 +0300

    Refactor code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mail-Bot
-With this python script, you will be able to send emails from a excel file in a simple way. You should use your gmail account for sending any mails.
+
+With this Python script, you will be able to send emails from a excel file in a simple way. You should use your Gmail account for sending any mails.
--- mail.py
+++ mail.py
@@ -15,51 +15,41 @@ https://myaccount.google.com/lesssecureapps
 
 """
 
-
-df = pd.read_excel (r'Example.xlsx') 
-mailarray = []
-totalmail = 0
+df = pd.read_excel(r'Example.xlsx')
+mails = []
+sendedMailsCount = 0
 
 for index, row in df.iterrows():
-	mailarray.append(row['MAIL ADDRESSES'])
+    mails.append(row['MAIL ADDRESSES'])
 
-for i in mailarray:
-	message = MIMEMultipart()
+for i in mails:
+    message = MIMEMultipart()
+    message["From"] = "YOUR GMAİL ADDRESS WILL BE HERE"  # From
+    message["To"] = i  # To
+    message["Subject"] = "Subject"  # Subject
 
-	message["From"] =  "YOUR GMAİL ADDRESS WILL BE HERE" # From
-	
-	message["To"] = i # To
-
-	message["Subject"] = "Subject"  # Subject
-
-	# Content of our mail
-	text = """
+    # Content of our mail
+    text = """
 	
 	"""
 
-	text_body =  MIMEText(text,"plain") 
+    text_body = MIMEText(text, "plain")
+    message.attach(text_body)
+    try:
+        mail = smtplib.SMTP("smtp.gmail.com", 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login("YOUR GMAİL", "YOUR PASSWORD")  # your gmail and password
+        mail.sendmail(message["From"], message["To"],
+                      message.as_string())  # sending
+        print("Mail sent successfully.... " + str(message["To"]))
+        mail.close()
+    except:
+        traceback.print_exc()
+        sys.stderr.write("Mail delivery failed... " + str(message["To"]))
+        sys.stderr.flush()
+    sendedMailsCount = sendedMailsCount + 1
+    if(mails[-1] != i):
+        time.sleep(20)
 
-	message.attach(text_body) 
-
-	try:
-		mail =  smtplib.SMTP("smtp.gmail.com",587) 
-
-		mail.ehlo() 
-	 
-		mail.starttls() 
-
-		mail.login("YOUR GMAİL","YOUR PASSWORD") # your gmail and password
-
-		mail.sendmail(message["From"],message["To"],message.as_string())  # sending
-		print("Mail sent successfully.... " + str(message["To"]))
-		mail.close() 
-
-	except:
-		traceback.print_exc()
-		sys.stderr.write("Mail delivery failed... "+ str(message["To"]) ) 
-		sys.stderr.flush()
-	totalmail = totalmail + 1
-	if(mailarray[-1] != i):
-		time.sleep(20)
-	
-print("Number of mail sent: " + str(totalmail))	
+print("Number of mail sent: " + str(sendedMailsCount))
